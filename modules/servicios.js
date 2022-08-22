fbc10a7cae76bed0bc55a3ffd26828e2853954f3
Joaquin Akerman <joaquin_akerman@hotmail.com>
2022-08-22 10:12:15 -0300

    Agrega aviso
    Al cumplirse 1 año del ultimo servicio, se muestra un aviso en el vehículo

--- modules/servicios.js
+++ modules/servicios.js
@@ -1,5 +1,24 @@
 function verDetalles(link) {
-  window.location.href = link;
+  if (link === null) {
+    window.alert('por el momento hay mas detalles');
+  } else {
+    window.location.href = link;
+  }
 }
 
-export { verDetalles };
+function ordenarFechaParaComparar(arrayFecha) {
+  const fechaParaComparar = new Date(
+    arrayFecha[0],
+    arrayFecha[1] - 1, //los meses arrancan en 0
+    arrayFecha[2],
+  );
+  return fechaParaComparar;
+}
+function obtenerFechaActual() {
+  const fechaActual = new Date().toISOString().split('T');
+  const soloFecha = fechaActual[0].toString().split('-'); // devuelve yyyy-MM-dd
+  const fechaFinal = ordenarFechaParaComparar(soloFecha);
+  return fechaFinal;
+}
+
+export { verDetalles, ordenarFechaParaComparar, obtenerFechaActual };
